8fb45275fec39c6670507d77ee518509127ec4e9
Ivan Tkachuk <ivktac@gmail.com>
2024-10-09 13:55:21 +0300

    feat(die): Generate random value

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,20 @@
 import Die from "./Die";
 
 export default function App() {
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.ceil(Math.random() * 6));
+    }
+    return newDice;
+  }
+
   return (
     <main>
       <div className="dice__container">
-        <Die value="1" />
-        <Die value="2" />
-        <Die value="3" />
-        <Die value="4" />
-        <Die value="5" />
-        <Die value="6" />
-        <Die value="7" />
-        <Die value="8" />
-        <Die value="9" />
-        <Die value="0" />
+        {allNewDice().map((die, index) => (
+          <Die key={index} value={die} />
+        ))}
       </div>
     </main>
   );
