8e62d32470df266d46ab156d1155ba6c4554b2d2
Ivan Tkachuk <ivktac@gmail.com>
2024-10-09 14:46:19 +0300

    feat(die): Add option to hold die

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Dice from "./Dice";
+import Die from "./Die";
 import { nanoid } from "nanoid";
 
 export default function App() {
@@ -8,21 +8,41 @@ export default function App() {
   function allNewDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
-      newDice.push({
-        value: Math.ceil(Math.random() * 6),
-        isHeld: false,
-        id: nanoid(),
-      });
+      newDice.push(generateNewDie());
     }
     return newDice;
   }
 
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    };
+  }
+
   function rollDice() {
-    setDice(allNewDice());
+    setDice((oldDice) => {
+      return oldDice.map((die) => (die.isHeld ? die : generateNewDie()));
+    });
+  }
+
+  function holdDice(id) {
+    setDice((oldDice) => {
+      return oldDice.map((die) =>
+        die.id === id ? { ...die, isHeld: !die.isHeld } : die,
+      );
+    });
   }
 
   const diceElements = dice.map((die) => (
-    <Dice key={die.id} value={die.value} isHeld={die.isHeld} />
+    <Die
+      key={die.id}
+      value={die.value}
+      isHeld={die.isHeld}
+      id={die.id}
+      holdDice={() => holdDice(die.id)}
+    />
   ));
 
   return (
